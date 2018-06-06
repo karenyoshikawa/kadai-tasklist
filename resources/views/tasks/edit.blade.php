9497c71a609e5dae06b544521c13e2ff50cee434
karen.yoshikawa <karen.yoshikawa@rakuten.com>
2018-06-06 04:16:19 +0000

    bootstrap design2

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -4,8 +4,8 @@
 
 <h1>id: {{ $task->id }} のタスク編集ページ</h1>
     <div class="row">
-    <div class="col-xs-12" "col-sm-offset-2 col-sm-8" "col-sm-offset-2 col-md-8" "col-sm-offset-3 col-lg-6">
-        
+        <div class="col-xs-12 col-sm-offset-2 col-sm-8  col-lg-offset-3 col-lg-6">
+   
     {!! Form::model($task, ['route' => ['tasks.update', $task->id], 'method' => 'put']) !!}
     
                 <div class="form-group">
@@ -18,7 +18,7 @@
                     {!! Form::text('content', null, ['class' => 'form-control']) !!}
                 </div>
     
-{!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
+　　　{!! Form::submit('更新', ['class' => 'btn btn-default']) !!}
         
     {!! Form::close() !!}
     </div>
